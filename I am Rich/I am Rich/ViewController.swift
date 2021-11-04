3c32e657913b02de207b9a6d5d552879c30cef7b
Yogi N. Patel <93512321+yogi272403@users.noreply.github.com>
2021-11-04 10:11:46 +0530

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Yogi Patel on 02/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
